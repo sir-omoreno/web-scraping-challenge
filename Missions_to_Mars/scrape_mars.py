970f2acae863e0d40de33472b018a38ef9f8927c
sir-omoreno <ozmoreno@gmail.com>
2021-03-14 01:30:39 -0500

    app.py FACTS section working

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -24,16 +24,30 @@ def scrape():
     articles = soup.find('ul', class_='item_list')
     news_title = articles.find('div', class_='content_title').text
     news_p = soup.find('div', class_='article_teaser_body').text
-    print(news_title)
-    print(news_p)
+    # print(news_title)
+    # print(news_p)
+    browser.quit()
+
+#### MARS FACTS SECTIONS ####
+    browser = init_browser()
+    mars_facts_url = "https://space-facts.com/mars/"
+    browser.visit(mars_facts_url)
+    html = browser.html
+    soup = bs(html, 'html')
+    time.sleep(5)
+    tables_to_pandas = pd.read_html(mars_facts_url)
+    facts_table = tables_to_pandas[0]
+    facts_table.columns = ["Description", "Value"]
+    facts_table.set_index("Description", inplace=True)
+    table_html = facts_table.to_html()
 
     browser.quit()
 
     mars_data = {
         "news_title": news_title,
-        "news_p": news_p
+        "news_p": news_p,
         # "featured_image_url": featured_image_url,
-        # "mars_facts_html": mars_facts_html,
+        "fact_table": table_html
         # "hemisphere_image_urls": hemisphere_image_urls
     }
 
